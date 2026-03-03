87c07fa99d0eaf45c2de0d6786dc610f6c67e629
willmurphy-coder <william_murphy@student.davincischools.org>
2026-03-03 12:00:49 -0800

    Update BillieJean.rb

--- BillieJean.rb
+++ BillieJean.rb
@@ -20,3 +20,17 @@ live_loop :billie_jean do
   play :cs2
   sleep 0.5
 end
+
+sleep 8
+
+live_loop :drums do
+  sample :drum_cymbal_closed
+  sleep 2
+end
+
+sleep 8
+
+live_loop :drums2 do
+  sample :drum_snare_hard
+  sleep 4
+end
